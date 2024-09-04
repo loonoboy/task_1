7803f9e5eb9b9fb1db7636c16a559334e3b5ee6d
loonoboy <tzimlov.max@yandex.ru>
2024-09-04 21:14:06 +0300

    update save func

--- task_1.py
+++ task_1.py
@@ -1,5 +1,6 @@
 import paramiko
 from tkinter import *
+from tkinter import ttk
 import getpass
 import os
 
@@ -17,20 +18,22 @@ def get_disk_info(hostname,port, username, password):
         return None
 
 #функция записи в txt файл
-def save_data(selected_items, filename):
-    if not os.path.exists(filename):
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
+def save_data(selected_items, entry1, entry2):
+    direct = entry1.get()
+    filename = entry2.get()
+    if not os.path.exists(direct):
+        os.makedirs(os.path.dirname(direct), exist_ok=True)
     with open(filename, 'w',encoding='utf-8') as file:
         file.write('\n'.join(selected_items))
 
 #выбор данный для сохранения, их сохранение
-def save_selected_items(listbox):
+def save_selected_items(listbox, entry1, entry2):
     selected_items = []
     for index in listbox.curselection():
         selected_item = listbox.get(index)
         if selected_item not in selected_items:
             selected_items.append(selected_item)
-    save_data(selected_items, "disk_info.txt")
+    save_data(selected_items, entry1, entry2)
 
 #основная
 def main():
@@ -46,15 +49,23 @@ def main():
         print(f"{output[i]}")
 
     root = Tk()
-
+    entry1 = Entry(root, width=40)
+    entry2 = Entry(root, width=40)
     # Создаём список с информацией о накопителях
-    listbox = Listbox(root,width=40, height=20, selectmode="extended")
+    listbox = Listbox(root, width=40, height=20, selectmode="extended")
     for i in range(len(output)):
         listbox.insert(END, f"{output[i]}")
-    listbox.pack()
     
     # Добавляем кнопку для сохранения выбранных элементов
-    button = Button(root, text="Сохранить", command=lambda: save_selected_items(listbox))
+    button = Button(root, text="Сохранить", command=lambda: save_selected_items(listbox, entry1, entry2))
+    
+    listbox.pack(anchor='center')
+    label1 = ttk.Label(text="Введите директорию сохранения")
+    label1.pack()
+    entry1.pack()
+    label2= ttk.Label(text="Введите название файла вместе с расширением")
+    label2.pack()
+    entry2.pack()
     button.pack()
 
     root.mainloop()
@@ -62,4 +73,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
